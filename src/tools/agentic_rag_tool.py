3cf95091811a340f40f7c808499696ab1b22dde6
AleSandler <AleGallagher@users.noreply.github.com>
2025-06-30 23:21:32 +0200

    adds historical matches, stats matches and fix rag for historical knowldge

--- src/tools/agentic_rag_tool.py
+++ src/tools/agentic_rag_tool.py
@@ -3,7 +3,7 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from rag.agentic_rag import AgenticRAG
 
-@tool("agentic_rag", return_direct=True, description="Use this for general knowledge questions or open-ended information about the Women's Eurocup 2025. Example: 'What can you say about Spain?' or 'Is there VAR?' or 'When will start the cup?'")
+@tool("agentic_rag", return_direct=True, description="Use this for general, historical knowledge questions or open-ended information about the Women's Eurocup 2025. Example: 'What can you say about Spain?' or 'Is there VAR?' or 'When will start the cup? or Top Goal Scorers in the hostory'")
 def agentic_rag_stream(vector_store, question: str = "", language: str = "English"):
     """
     Retrieve relevant documents based on the query.
