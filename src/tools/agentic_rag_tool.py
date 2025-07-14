81e10f1987895bdabd66b5f0751cbe0725c3b31d
AleSandler <AleGallagher@users.noreply.github.com>
2025-07-14 21:16:26 +0200

    improve routing

--- src/tools/agentic_rag_tool.py
+++ src/tools/agentic_rag_tool.py
@@ -3,7 +3,16 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from rag.agentic_rag import AgenticRAG
 
-@tool("agentic_rag", return_direct=True, description="Use this for general, historical knowledge questions or open-ended information about the Women's Eurocup 2025. Example: 'What can you say about Spain?' or 'Is there VAR?' or 'When will start the cup? or Top Goal Scorers in the hostory'")
+@tool("agentic_rag", return_direct=True, description=(
+                    "Use this tool for general background, historical knowledge, or open-ended questions "
+                    "about the Women's Eurocup 2025. This includes rules (e.g., VAR), past tournaments, hosts, and top scorers in history. "
+                    "Examples:\n"
+                    "- 'What can you say about Spain?'\n"
+                    "- 'Is there VAR?'\n"
+                    "- 'When does the cup start?'\n"
+                    "- 'Top goal scorers in tournament history?'"
+                )
+)
 def agentic_rag_stream(vector_store, question: str = "", language: str = "English"):
     """
     Retrieve relevant documents based on the query.
